9cc08647a53913a1674a14756107cbc57c648178
Milagros Sofia Perez <mimisofiap21@gmail.com>
2025-11-16 22:58:51 -0300

    fix: detalles presupuesto

--- Repositories/PresupuestosRepository.cs
+++ Repositories/PresupuestosRepository.cs
@@ -32,6 +32,33 @@ public class PresupuestosRepository
         }
     }
 
+    public bool updatePresupuesto(Presupuestos Presupuesto)
+    {
+        try
+        {
+            using (var connection = new SqliteConnection(connectionString))
+            {
+                connection.Open();
+                string sql = "UPDATE Presupuestos SET Descripcion = @desc, Precio = @prec WHERE idPresupuesto = @id";
+
+                using (var command = new SqliteCommand(sql, connection))
+                {
+                    command.Parameters.AddWithValue("@desc", Presupuesto.nombreDestinatario);
+                    command.Parameters.AddWithValue("@prec", Presupuesto.fechaCreacion);
+                    command.Parameters.AddWithValue("@id", Presupuesto.idPresupuesto);
+
+                    int rows = command.ExecuteNonQuery();
+                    return rows > 0;
+                }
+            }
+        }
+        catch
+        {
+
+            return false;
+        }
+    }
+
     public List<Presupuestos> getPresupuestos()
     {
         var ListaPresupuestos = new List<Presupuestos>();
@@ -63,32 +90,62 @@ public class PresupuestosRepository
         return ListaPresupuestos;
     }
 
-    public List<PresupuestosDetalle> getDetallesPresupuesto(int id)
+    public Presupuestos? getDetallesPresupuesto(int id)
     {
-        var listaDetalles = new List<PresupuestosDetalle>();
         using (var conexion = new SqliteConnection(connectionString))
         {
             conexion.Open();
-            string consulta = "SELECT * FROM Presupuestos INNER JOIN PresupuestosDetalle using(IdPresupuesto) INNER JOIN Productos using(IdProducto) WHERE IdPresupuesto = @Id";
+
+            string consulta = @"
+                SELECT  p.IdPresupuesto, p.NombreDestinatario, p.FechaCreacion, pd.IdProducto, pd.Cantidad, pr.Descripcion, pr.Precio
+                FROM Presupuestos p
+                LEFT JOIN PresupuestosDetalle pd USING (IdPresupuesto)
+                LEFT JOIN Productos pr USING (IdProducto)
+                WHERE p.IdPresupuesto = @Id;";
 
             var comando = new SqliteCommand(consulta, conexion);
             comando.Parameters.Add(new SqliteParameter("@Id", id));
 
-            using var lector = comando.ExecuteReader();
+            using var reader = comando.ExecuteReader();
 
-            while (lector.Read())
+            Presupuestos? presupuesto = null;
+            var detalles = new List<PresupuestosDetalle>();
+
+            while (reader.Read())
             {
-                var producto = new Productos(Convert.ToInt32(lector["IdProducto"]), lector["Descripcion"].ToString(), Convert.ToInt32(lector["Precio"]));
+                
+                if (presupuesto == null)
+                {
+                    presupuesto = new Presupuestos();
+                    presupuesto.idPresupuesto = Convert.ToInt32(reader["idPresupuesto"]);
+                    presupuesto.nombreDestinatario = reader["NombreDestinatario"].ToString();
 
-                var detalle = new PresupuestosDetalle(producto, Convert.ToInt32(lector["Cantidad"]));
+                    var iFecha = reader.GetOrdinal("FechaCreacion");
+                    presupuesto.fechaCreacion = DateOnly.FromDateTime(reader.GetDateTime(iFecha));
+                    presupuesto.detalle = detalles;
+                }
 
-                listaDetalles.Add(detalle);
+                if (!reader.IsDBNull(reader.GetOrdinal("IdProducto")))
+                {
+                    var producto = new Productos(
+                        Convert.ToInt32(reader["IdProducto"]),
+                        reader["Descripcion"]?.ToString(),
+                        Convert.ToInt32(reader["Precio"])
+                    );
+
+                    var detalle = new PresupuestosDetalle(
+                        producto,
+                        Convert.ToInt32(reader["Cantidad"])
+                    );
+
+                    detalles.Add(detalle);
+                }
             }
 
+            return presupuesto;
         }
-        return listaDetalles;
-
     }
+
 
     public bool agregarDetalle(int idPresupuesto, int idProducto, int cantidad)
     {
