ec6cf9e76c0fff8bce82d4c69e50d1bc4c0609aa
MiliSPZ <mimisofiap21@gmail.com>
2025-11-17 17:05:26 -0300

    viewmodel agregar producto

---
+++ ViewModels/AgregarProductoViewModel.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.Rendering; 
+
+namespace Sistema.Web.ViewModels;
+
+public class AgregarProductoViewModel
+{
+    public int idPresupuesto { get; set; }
+
+    [Display(Name = "Producto a agregar")]
+    public int IdProducto { get; set; }
+
+    [Display(Name = "Cantidad")]
+    [Required(ErrorMessage = "La cantidad es obligatoria.")]
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor a cero.")]
+    public int Cantidad { get; set; }
+
+    public SelectList ListaProductos { get; set; }
+}
