d6cba3321307ac60848e714b313c522c9d741d27
Milagros Sofia Perez <mimisofiap21@gmail.com>
2025-11-20 00:18:16 -0300

    modificacion acceso

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,21 +1,37 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using tl2_tp8_2025_MiliSPZ.Models;
+using Sistema.Web.Interfaces;
 
 namespace tl2_tp8_2025_MiliSPZ.Controllers;
 
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private IAuthenticationService _authService;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, IAuthenticationService authService)
     {
         _logger = logger;
+        _authService = authService;
     }
 
     public IActionResult Index()
     {
-        return View();
+        if (!_authService.IsAuthenticated())
+        {
+            return RedirectToAction("Index", "Login");
+        }
+
+        if (_authService.HasAccessLevel("Administrador") || _authService.HasAccessLevel("Cliente") )
+        {
+            //si es es valido entra sino vuelve a login
+            return View();
+        }
+        else
+        {
+            return RedirectToAction("Index", "Login");
+        }
     }
 
     public IActionResult Privacy()
