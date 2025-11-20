1724a11a2870bbcef0337e186a5ebb6882817441
Milagros Sofia Perez <mimisofiap21@gmail.com>
2025-11-19 21:24:38 -0300

    modelo Usuario

---
+++ Models/Usuario.cs
@@ -0,0 +1,22 @@
+namespace Sistema.Web.Models;
+
+public class Usuario
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string User { get; set; }
+    public string Pass { get; set; }
+    public string Rol { get; set; }
+
+    // Constructores
+    public Usuario(int id, string nombre, string user, string pass, string rol)
+    {
+        Id = id;
+        Nombre = nombre;
+        User = user;
+        Pass = pass;
+        Rol = rol;
+    }
+
+    public Usuario() { }
+}
